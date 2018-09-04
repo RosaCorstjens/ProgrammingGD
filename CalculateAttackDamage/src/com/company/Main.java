6b0d80e6305f9a0a3226aeb60a6ee0bbd76abfa9
Rosa Corstjens <rosa.corstjens@gmail.com>
2018-09-04 20:01:37 +0200

    Added another calculation and a cast from double to int to practicum 2

--- CalculateAttackDamage/src/com/company/Main.java
+++ CalculateAttackDamage/src/com/company/Main.java
@@ -15,6 +15,9 @@ public class Main {
         final int MAX_HEALTH = 100;
         float currentHealth = MAX_HEALTH;
 
+        int totalAmountOfAttacks;
+        float totalAmountOfDamage;
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter Pickachu's base damage: ");
@@ -25,8 +28,14 @@ public class Main {
 
         resultingDamagePickachu = ((baseDamagePickachu / 100) * effectiveProcentPickachu);
         currentHealth = MAX_HEALTH - resultingDamagePickachu;
-        System.out.println("The other pokemon starts with " + MAX_HEALTH + " health.\n" +
+        System.out.println("\nThe other pokemon starts with " + MAX_HEALTH + " health.\n" +
                 "Pickachu deals " + resultingDamagePickachu + " damage.\n"+
                 "The other pokemon is left with " + currentHealth + " health.");
+
+        totalAmountOfAttacks = (int)Math.ceil(MAX_HEALTH / resultingDamagePickachu);
+        totalAmountOfDamage = totalAmountOfAttacks * resultingDamagePickachu;
+
+        System.out.println("\nPickachu would need to attack " + totalAmountOfAttacks + " times to defeat the other pokemon.\n"+
+                "Pickachu would then deal " + totalAmountOfDamage + " damage.");
     }
 }
