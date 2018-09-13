349730f1507b0d56a5e886ed92b34f37b76a0bcd
RosaCorstjens <rosa.corstjens@gmail.com>
2018-09-13 21:27:47 +0200

    Finishing practicum 3 (minor changes while typing handout)

--- CalculateAttackDamage2/src/com/company/Main.java
+++ CalculateAttackDamage2/src/com/company/Main.java
@@ -9,46 +9,45 @@ public class Main {
     public static void main(String[] args) {
         final String NAME_PIKACHU = "Pikachu";
         final String ATTACK_PIKACHU = "Thunder shock";
-        final double POWER_ATTACK_PIKACHU = 40;
-        final double BASE_HEALTH_PIKACHU = 35;
-        final double BASE_ATTACK_PIKACHU = 55;
+        final int POWER_ATTACK_PIKACHU = 40;
+        final int BASE_HEALTH_PIKACHU = 35;
+        final int BASE_ATTACK_PIKACHU = 55;
         double healthPikachu;
-        double currentHealthPikachu;
         double attackPikachu;
         int levelPikachu;
 
         final String NAME_BULBASAUR = "Bulbasaur";
         final String ATTACK_BULBASAUR = "Vine whip";
-        final double POWER_ATTACK_BULBASAUR = 45;
-        final double BASE_HEALTH_BULBASAUR = 45;
-        final double BASE_ATTACK_BULBASAUR = 49;
+        final int POWER_ATTACK_BULBASAUR = 45;
+        final int BASE_HEALTH_BULBASAUR = 45;
+        final int BASE_ATTACK_BULBASAUR = 49;
         double healthBulbasaur;
         double attackBulbasaur;
         int levelBulbasaur;
 
         final String NAME_SQUIRTLE = "Squirtle";
         final String ATTACK_SQUIRTLE = "Bubble";
-        final double POWER_ATTACK_SQUIRTLE = 40;
-        final double BASE_HEALTH_SQUIRTLE = 44;
-        final double BASE_ATTACK_SQUIRTLE = 48;
+        final int POWER_ATTACK_SQUIRTLE = 40;
+        final int BASE_HEALTH_SQUIRTLE = 44;
+        final int BASE_ATTACK_SQUIRTLE = 48;
         double healthSquirtle;
         double attackSquirtle;
         int levelSquirtle;
 
         final String NAME_CHARMANDER = "Charmander";
         final String ATTACK_CHARMANDER = "Ember";
-        final double POWER_ATTACK_CHARMANER = 40;
-        final double BASE_HEALTH_CHARMANDER = 39;
-        final double BASE_ATTACK_CHARMANDER = 52;
+        final int POWER_ATTACK_CHARMANER = 40;
+        final int BASE_HEALTH_CHARMANDER = 39;
+        final int BASE_ATTACK_CHARMANDER = 52;
         double healthCharmander;
         double attackCharmander;
         int levelCharmander;
 
         final String NAME_PIDGEY = "Pidgey";
         final String ATTACK_PIDGEY = "Quick Attack";
-        final double POWER_ATTACK_PIDGEY = 40;
-        final double BASE_HEALTH_PIDGEY = 40;
-        final double BASE_ATTACK_PIDGEY = 45;
+        final int POWER_ATTACK_PIDGEY = 40;
+        final int BASE_HEALTH_PIDGEY = 40;
+        final int BASE_ATTACK_PIDGEY = 45;
         double healthPidgey;
         double attackPidgey;
         int levelPidgey;
@@ -74,22 +73,22 @@ public class Main {
         levelPidgey = input.nextInt();
 
         // calculate stats based on level
-        // (simplified) formula HP: floor(floor(2*base)*level/100+level+10)
-        // (simplified) formula attack: floor(floor(2*base)*level/100+5)
-        healthPikachu = Math.floor(Math.floor(2 * BASE_HEALTH_PIKACHU) * levelPikachu / 100 + levelPikachu + 10);
-        attackPikachu = Math.floor(Math.floor(2 * BASE_ATTACK_PIKACHU) * levelPikachu / 100 + 5);
+        // (simplified) formula HP: floor(2*base*level/100+level+10)
+        // (simplified) formula attack: floor(2*base*level/100+5)
+        healthPikachu = Math.floor((2 * BASE_HEALTH_PIKACHU * levelPikachu / 100f + levelPikachu + 10) * 100) / 100;
+        attackPikachu = Math.floor((2 * BASE_ATTACK_PIKACHU * levelPikachu / 100f + 5)* 100) / 100;
 
-        healthBulbasaur = Math.floor(Math.floor(2 * BASE_HEALTH_BULBASAUR) * levelBulbasaur / 100 + levelBulbasaur + 10);
-        attackBulbasaur = Math.floor(Math.floor(2 * BASE_ATTACK_BULBASAUR) * levelBulbasaur / 100 + 5);
+        healthBulbasaur = Math.floor((2 * BASE_HEALTH_BULBASAUR * levelBulbasaur / 100f + levelBulbasaur + 10) * 100) / 100;
+        attackBulbasaur = Math.floor((2 * BASE_ATTACK_BULBASAUR * levelBulbasaur / 100f + 5) * 100) / 100;
 
-        healthSquirtle = Math.floor(Math.floor(2 * BASE_HEALTH_SQUIRTLE) * levelSquirtle / 100 + levelSquirtle + 10);
-        attackSquirtle = Math.floor(Math.floor(2 * BASE_ATTACK_SQUIRTLE) * levelSquirtle / 100 + 5);
+        healthSquirtle = Math.floor((2 * BASE_HEALTH_SQUIRTLE * levelSquirtle / 100f + levelSquirtle + 10) * 100) / 100;
+        attackSquirtle = Math.floor((2 * BASE_ATTACK_SQUIRTLE * levelSquirtle / 100f + 5) * 100) / 100;
 
-        healthCharmander = Math.floor(Math.floor(2 * BASE_HEALTH_CHARMANDER) * levelCharmander / 100 + levelCharmander + 10);
-        attackCharmander = Math.floor(Math.floor(2 * BASE_ATTACK_CHARMANDER) * levelCharmander / 100 + 5);
+        healthCharmander = Math.floor((2 * BASE_HEALTH_CHARMANDER * levelCharmander / 100f + levelCharmander + 10) * 100) / 100;
+        attackCharmander = Math.floor((2 * BASE_ATTACK_CHARMANDER * levelCharmander / 100f + 5) * 100) / 100;
 
-        healthPidgey = Math.floor(Math.floor(2 * BASE_HEALTH_PIDGEY) * levelPidgey / 100 + levelPidgey + 10);
-        attackPidgey = Math.floor(Math.floor(2 * BASE_ATTACK_PIDGEY) * levelPidgey / 100 + 5);
+        healthPidgey = Math.floor((2 * BASE_HEALTH_PIDGEY * levelPidgey / 100f + levelPidgey + 10) * 100) / 100;
+        attackPidgey = Math.floor((2 * BASE_ATTACK_PIDGEY * levelPidgey / 100f + 5) * 100) / 100;
 
         // print all stats
         System.out.println("\n" + NAME_PIKACHU + "\t\t Level: " + levelPikachu + "\t Attack: " + attackPikachu + "\t HP: " + healthPikachu + "\n" +
@@ -123,42 +122,42 @@ public class Main {
         double resultingDamage = 0;
         switch(chosenPokemon){
             case 0:
-                resultingDamage = (((levelPikachu / 5 + 2) * POWER_ATTACK_PIKACHU * attackPikachu) / 100) + 2;
+                resultingDamage = Math.round((((((levelPikachu / 5f + 2) * POWER_ATTACK_PIKACHU * attackPikachu) / 100) + 2) * 100 ) / 100);
                 healthCaterpie -= resultingDamage;
 
-                System.out.println("\n" + NAME_PIKACHU + "uses " + ATTACK_PIKACHU + " against Caterpie! \n" +
+                System.out.println("\n" + NAME_PIKACHU + " uses " + ATTACK_PIKACHU + " against Caterpie! \n" +
                         NAME_PIKACHU + " deals " + resultingDamage + " damage! \n" +
                         "Caterpie is left with " + healthCaterpie + " health.");
                 break;
             case 1:
-                resultingDamage = (((levelBulbasaur / 5 + 2) * POWER_ATTACK_BULBASAUR * attackBulbasaur) / 100) + 2;
+                resultingDamage = Math.round((((((levelBulbasaur / 5f + 2) * POWER_ATTACK_BULBASAUR * attackBulbasaur) / 100) + 2) * 100 ) / 100);
                 healthCaterpie -= resultingDamage;
 
-                System.out.println("\n" + NAME_BULBASAUR + "uses " + ATTACK_BULBASAUR + " against Caterpie! \n" +
+                System.out.println("\n" + NAME_BULBASAUR + " uses " + ATTACK_BULBASAUR + " against Caterpie! \n" +
                         NAME_BULBASAUR + " deals " + resultingDamage + " damage! \n" +
                         "Caterpie is left with " + healthCaterpie + " health.");
                 break;
             case 2:
-                resultingDamage = (((levelSquirtle / 5 + 2) * POWER_ATTACK_SQUIRTLE * attackSquirtle) / 100) + 2;
+                resultingDamage = Math.round((((((levelSquirtle / 5f + 2) * POWER_ATTACK_SQUIRTLE * attackSquirtle) / 100) + 2) * 100 ) / 100);
                 healthCaterpie -= resultingDamage;
 
-                System.out.println("\n" + NAME_SQUIRTLE + "uses " + ATTACK_SQUIRTLE + " against Caterpie! \n" +
+                System.out.println("\n" + NAME_SQUIRTLE + " uses " + ATTACK_SQUIRTLE + " against Caterpie! \n" +
                         NAME_SQUIRTLE + " deals " + resultingDamage + " damage! \n" +
                         "Caterpie is left with " + healthCaterpie + " health.");
                 break;
             case 3:
-                resultingDamage = (((levelCharmander / 5 + 2) * POWER_ATTACK_CHARMANER * attackCharmander) / 100) + 2;
+                resultingDamage = Math.round((((((levelCharmander / 5f + 2) * POWER_ATTACK_CHARMANER * attackCharmander) / 100) + 2) * 100 ) / 100);
                 healthCaterpie -= resultingDamage;
 
-                System.out.println("\n" + NAME_CHARMANDER + "uses " + ATTACK_CHARMANDER + " against Caterpie! \n" +
+                System.out.println("\n" + NAME_CHARMANDER + " uses " + ATTACK_CHARMANDER + " against Caterpie! \n" +
                         NAME_CHARMANDER + " deals " + resultingDamage + " damage! \n" +
                         "Caterpie is left with " + healthCaterpie + " health.");
                 break;
             case 4:
-                resultingDamage = (((levelPidgey / 5 + 2) * POWER_ATTACK_PIDGEY * attackPidgey) / 100) + 2;
+                resultingDamage = Math.round((((((levelPidgey / 5f + 2) * POWER_ATTACK_PIDGEY * attackPidgey) / 100) + 2) * 100 ) / 100);
                 healthCaterpie -= resultingDamage;
 
-                System.out.println("\n" + NAME_PIDGEY + "uses " + ATTACK_PIDGEY + " against Caterpie! \n" +
+                System.out.println("\n" + NAME_PIDGEY + " uses " + ATTACK_PIDGEY + " against Caterpie! \n" +
                         NAME_PIDGEY + " deals " + resultingDamage + " damage! \n" +
                         "Caterpie is left with " + healthCaterpie + " health.");
                 break;
